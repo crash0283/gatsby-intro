faa32b5781f1caf1f593f0a14116867fea132146
crash0283 <rasch83@gmail.com>
2020-01-19 09:28:46 -0600

    still fixing

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,9 +9,9 @@ import { Auth0Provider } from "./src/auth0/auth"
 export const wrapRootElement = ({ element }) => {
   return (
     <Auth0Provider
-      domain={process.env.AUTH_DOMAIN}
-      client_id={process.env.AUTH_CLIENT_ID}
-      redirect_uri={process.env.AUTH_REDIRECT_URL}
+      domain={`dev-pnkhb30m.auth0.com`}
+      client_id={`2feEfVzpSLhghw5BhCuVowmfc6SBh4d5`}
+      redirect_uri={`https://unruffled-darwin-677325.netlify.com/blog`}
     >
       {element}
     </Auth0Provider>
