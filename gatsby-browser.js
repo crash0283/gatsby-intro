223d3fa39968dfac88322ac6f606e94992ee60f8
crash0283 <rasch83@gmail.com>
2020-01-20 12:14:15 -0600

    working blog app using contentful and auth0

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,9 +7,6 @@ import React from "react"
 import { Auth0Provider } from "./src/auth0/auth"
 
 export const wrapRootElement = ({ element }) => {
-  //`https://unruffled-darwin-677325.netlify.com/blog`
-  //`2feEfVzpSLhghw5BhCuVowmfc6SBh4d5`
-  //`dev-pnkhb30m.auth0.com`
   return (
     <Auth0Provider
       domain={process.env.GATSBY_AUTH_DOMAIN}
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,9 +7,6 @@ import React from "react"
 import { Auth0Provider } from "./src/auth0/auth"
 
 export const wrapRootElement = ({ element }) => {
-    // `dev-pnkhb30m.auth0.com`
-    // `2feEfVzpSLhghw5BhCuVowmfc6SBh4d5`
-    // `https://unruffled-darwin-677325.netlify.com/blog`
   return (
     <Auth0Provider
       domain={process.env.GATSBY_AUTH_DOMAIN}
